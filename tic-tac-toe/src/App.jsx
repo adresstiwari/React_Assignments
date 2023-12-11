6ab7c250ce8745760ae9f34f307b093188ad7624
Adarsh Tiwari <122097352+adresstiwari@users.noreply.github.com>
2023-12-11 22:22:42 +0530

    Add files via upload

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Grid from './components/Grid/Grid'
+import Card from './components/card/card'
+
+
+function App() {
+
+  return (
+   <>
+   
+    <Grid numberOfCards={9}  /> 
+   </>
+  )
+}
+
+export default App
